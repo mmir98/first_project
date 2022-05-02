d36ff1dc3101ac6bc57396c995a24171e3649cf8
mohammad <mohammadmir9891@gmail.com>
2022-05-02 15:27:21 +0430

    row/column validators implemented

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
-#include<stdio.h>
-#include<stdbool.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
 
-bool format_input(char*, char*, int*, int*);
-
+bool format_input(char *, char *, int *, int *);
+bool row_validation(int width, int height, char [width][height]);
+bool column_validation(int width, int height, char [width][height]);
+void subRect_validation();
 
 int main(int argc, char const *argv[])
 {
@@ -20,25 +23,55 @@ int main(int argc, char const *argv[])
         return 0;
     }
 
-    int dimensions [nums];
+    int board_width, board_height;
+    int subrects_width[(nums - 2) / 2], subrects_height[(nums - 2) / 2];
+
     FILE *input_fp = fopen("formatted_input.txt", "r");
-    char buffer [buffer_size];
+    char buffer[buffer_size];
     int count = fread(&buffer, sizeof(char), buffer_size, input_fp);
-    
     char num_delim[] = "*";
-    int flag = 1;
     char *st_ptr = strtok(buffer, num_delim);
-    while (st_ptr != NULL || flag <= nums);
+    int flag = 1;
+
+    while (st_ptr != NULL && flag <= nums)
     {
-        printf("%s\n", st_ptr);
+        if (flag == 1)
+        {
+            // board width
+            // printf("width :: %d\n", atoi(st_ptr));
+            board_width = atoi(st_ptr);
+        }
+        if (flag == 2)
+        {
+            // board height
+            // printf("height :: %d\n", atoi(st_ptr));
+            board_height = atoi(st_ptr);
+        }
+        if (flag > 2 && flag <= 2 + (nums - 2) / 2)
+        {
+            // sub-rect width
+            // printf("sub-rect width :: %d\n", atoi(st_ptr));
+            subrects_width[flag - 3] = atoi(st_ptr);
+        }
+        if (flag > 2 + (nums - 2) / 2 && flag <= nums)
+        {
+            // sub-rect height
+            // printf("sub-rect height  :: %d\n", atoi(st_ptr));
+            subrects_height[flag - 3 - (nums - 2) / 2] = atoi(st_ptr);
+        }
+        // printf("%s\n", st_ptr);
         st_ptr = strtok(NULL, num_delim);
+        flag += 1;
     }
-    
-    
+
+    char board[board_width][board_height];
+    buffer_size = board_width + 1;
+    int count2 = fread(&buffer, sizeof(char), buffer_size, input_fp);
+    printf("%s\n", buffer);
+
     fclose(input_fp);
 
-
-    //! format_input func test
+    //! tests
     // TODO remove
     input_fp = fopen("formatted_input.txt", "r");
     char c;
@@ -54,16 +87,26 @@ int main(int argc, char const *argv[])
     {
         printf("%c", buffer[i]);
     }
-    
+
     printf("\n");
     printf("count :: %d\n%ld\n", count, sizeof(int));
-    
+
+    printf("board width :: %d\n", board_width);
+    printf("board height :: %d\n", board_height);
+    for (int i = 0; i < sizeof(subrects_width) / sizeof(int); i++)
+    {
+        printf("subrect %d width :: %d\n", i, subrects_width[i]);
+        printf("subrect %d height :: %d\n", i, subrects_height[i]);
+    }
+
+    printf("%d\n", ('Z' - 'A'));
+
     return 0;
 }
 
-
 //* Used to remove spaces and newline chars from input file
-bool format_input(char *input_fn, char *output_fn, int *nums, int *buffer_size){
+bool format_input(char *input_fn, char *output_fn, int *nums, int *buffer_size)
+{
     FILE *input_fp = fopen(input_fn, "r");
     FILE *formatted_input_fp = fopen(output_fn, "w");
     if (NULL == input_fp)
@@ -76,11 +119,11 @@ bool format_input(char *input_fn, char *output_fn, int *nums, int *buffer_size){
         printf("Can't open formatted file");
         return false;
     }
-    
+
     char c;
     while ((c = fgetc(input_fp)) != EOF)
     {
-        if (c == ' ' || c == 10 || c == 13)
+        if (c == ' ' || c == 9 || c == 10 || c == 13)
         {
             continue;
         }
@@ -92,12 +135,63 @@ bool format_input(char *input_fn, char *output_fn, int *nums, int *buffer_size){
         if (c >= '0' && c <= '9')
         {
             *buffer_size += 1;
-        }    
+        }
 
         fputc(c, formatted_input_fp);
     }
-    
+
     fclose(input_fp);
     fclose(formatted_input_fp);
     return true;
 }
+
+//* Validates all rows within the given board. return true if validation succeeds and false o.w.
+bool row_validation(int width, int height, char board[width][height])
+{
+    int flag[26];    //! Either 0 or 1
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int index = board[j][i] - 'a';
+            if (flag[index] == 1)
+            {
+                /* validation failed */
+               return false;
+            }
+            else {
+                flag[index] = 1;
+            }  
+        }
+    }
+    /* validation succeed */
+    return true;
+}
+
+//* Validate all columns within the given board. return true if validation succeeds and false o.w.
+bool column_validation(int width, int height, char board[width][height])
+{
+    int flag[26];   //! Either 0 or 1
+    for (int i = 0; i < width; i++)
+    {
+        for (int j = 0; j < height; j++)
+        {
+            int index = board[i][j] - 'A';
+            if (flag[index] == 1)
+            {
+                /* validation failed */
+               return false;
+            }
+            else {
+                flag[index] = 1;
+            }  
+        }
+    }
+    /* validation succeed */
+    return true;
+}
+
+void subRect_validation()
+{
+}
+
