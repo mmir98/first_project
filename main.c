522fd81e10ca7d1a8a61ba9b49a59b41a991b3b8
mohammad <mohammadmir9891@gmail.com>
2022-05-05 19:53:42 +0430

    RVP fixed. CVP and SVP implemented.

--- main.c
+++ main.c
@@ -7,9 +7,13 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 
-#define FIFO_PATH "/home/mohammad/Desktop/OS/first_project/fifo"
+#define FIFO_PATH "fifo"
 #define RVP_FAILED "rvp_f"
 #define RVP_SUCCEED "rvp_s"
+#define CVP_FAILED "cvp_f"
+#define CVP_SUCCEED "cvp_s"
+#define SVP_FAILED "svp_f"
+#define SVP_SUCCEED "svp_s"
 
 bool format_input(char *, char *, int *, int *);
 bool row_validation(int width, int height, char[width][height]);
@@ -18,6 +22,8 @@ bool subRect_validation(int width, int height, char[width][height], int, int);
 char decryptor(char);
 void decryptor_process(int width, int height, char[width][height]);
 void row_validator_process(int width, int height, char[width][height]);
+void column_validator_process(int width, int height, char[width][height]);
+void subRect_validator_process(int width, int height, char[width][height], int, int);
 
 int main(int argc, char const *argv[])
 {
@@ -163,13 +169,13 @@ int main(int argc, char const *argv[])
     if (rvp != 0 && cvp == 0 && svp != 0)
     {
         /* Column validator process */
-
+        column_validator_process(board_width, board_height, decrypted_board);
         exit(0);
     }
     if (rvp != 0 && cvp != 0 && svp == 0)
     {
         /* SubRect validator process */
-
+        subRect_validator_process(board_width, board_height, decrypted_board, subrects_width, subrects_height);
         exit(0);
     }
     if (rvp > 0 && cvp > 0 && svp > 0)
@@ -190,8 +196,26 @@ int main(int argc, char const *argv[])
             return 0;
         }
         printf("%s\n", res_buffer);
+        sleep(1);
+        if (read(fd, res_buffer, sizeof(RVP_SUCCEED)) == -1)
+        {
+            /*  */
+            printf("Parent can't read from FIFO");
+            return 0;
+        }
+        printf("%s\n", res_buffer);
+        sleep(1);
+        if (read(fd, res_buffer, sizeof(RVP_SUCCEED)) == -1)
+        {
+            /*  */
+            printf("Parent can't read from FIFO");
+            return 0;
+        }
+        printf("%s\n", res_buffer);
+        close(fd);
 
-        //! tests
+
+        //! debug tests
         // TODO remove
         // input_fp = fopen("formatted_input.txt", "r");
         // char c;
@@ -201,48 +225,46 @@ int main(int argc, char const *argv[])
         // }
         // printf("\n");
         // fclose(input_fp);
-        printf("%d : nums \n%d : buffer_size \n", nums, buffer_size);
+        // printf("%d : nums \n%d : buffer_size \n", nums, buffer_size);
 
-        for (int i = 0; i < count; i++)
-        {
-            printf("%c", buffer[i]);
-        }
+        // for (int i = 0; i < count; i++)
+        // {
+        //     printf("%c", buffer[i]);
+        // }
 
-        printf("\n");
-        printf("count :: %d\n", count);
+        // printf("\n");
+        // printf("count :: %d\n", count);
 
-        printf("board width :: %d\n", board_width);
-        printf("board height :: %d\n", board_height);
+        // printf("board width :: %d\n", board_width);
+        // printf("board height :: %d\n", board_height);
 
-        printf("subrect  width :: %d\n", subrects_width);
-        printf("subrect  height :: %d\n", subrects_height);
+        // printf("subrect  width :: %d\n", subrects_width);
+        // printf("subrect  height :: %d\n", subrects_height);
         // printf("%d\n", ('Z' - 'A'));
         // printf("%d - %d\n", 'a', 'z');
         // printf("%d - %c\n", 'A' - 2, 'Z'-1);
         // printf("%c\n", 67);
-        for (int i = 0; i < board_height; i++)
-        {
-            /* code */
-            for (int j = 0; j < board_width; j++)
-            {
-                /* code */
-                printf("%c ", mainBoard[j][i]);
-            }
-            printf("\n");
-        }
-        printf("\t\nDecrypted version!\n");
-        for (int i = 0; i < board_height; i++)
-        {
-            /* code */
-            for (int j = 0; j < board_width; j++)
-            {
-                /* code */
-                printf("%c ", decrypted_board[j][i]);
-            }
-            printf("\n");
-        }
-
-        return 0;
+        // for (int i = 0; i < board_height; i++)
+        // {
+        //     /* code */
+        //     for (int j = 0; j < board_width; j++)
+        //     {
+        //         /* code */
+        //         printf("%c ", mainBoard[j][i]);
+        //     }
+        //     printf("\n");
+        // }
+        // printf("\t\nDecrypted version!\n");
+        // for (int i = 0; i < board_height; i++)
+        // {
+        //     /* code */
+        //     for (int j = 0; j < board_width; j++)
+        //     {
+        //         /* code */
+        //         printf("%c ", decrypted_board[j][i]);
+        //     }
+        //     printf("\n");
+        // }
     }
     return 0;
 }
@@ -289,9 +311,9 @@ bool format_input(char *input_fn, char *output_fn, int *nums, int *buffer_size)
 //* Validates all rows within the given board. return true if validation succeeds and false o.w.
 bool row_validation(int width, int height, char board[width][height])
 {
-    int flag[26]; //! Either 0 or 1
     for (int i = 0; i < height; i++)
     {
+        int flag[26] = {0}; //! Either 0 or 1
         for (int j = 0; j < width; j++)
         {
             int index = board[j][i] - 'A';
@@ -313,9 +335,10 @@ bool row_validation(int width, int height, char board[width][height])
 //* Validate all columns within the given board. return true if validation succeeds and false o.w.
 bool column_validation(int width, int height, char board[width][height])
 {
-    int flag[26]; //! Either 0 or 1
+
     for (int i = 0; i < width; i++)
     {
+        int flag[26] = {0}; //! Either 0 or 1
         for (int j = 0; j < height; j++)
         {
             int index = board[i][j] - 'A';
@@ -366,8 +389,10 @@ bool subRect_validation(int width, int height, char board[width][height], int su
             }
         }
     }
+    return true;
 }
 
+//* Capitalise input_char and decrypt it.
 char decryptor(char input_char)
 {
     char res;
@@ -397,6 +422,7 @@ char decryptor(char input_char)
 }
 
 //* Child Processes
+//* Uses decryptor func to decrypt board parameter
 void decryptor_process(int width, int height, char board[width][height])
 {
     int fd = open(FIFO_PATH, O_WRONLY);
@@ -412,6 +438,9 @@ void decryptor_process(int width, int height, char board[width][height])
     close(fd);
 }
 
+//* Uses row_validator func to validate rows within the board parameter.
+//* If valid sends [RVP_SUCCEED] msg to parent.
+//* If invalid sends [RVP_FAILED] msg to parent.
 void row_validator_process(int width, int height, char board[width][height])
 {
     if (row_validation(width, height, board) == false)
@@ -448,10 +477,80 @@ void row_validator_process(int width, int height, char board[width][height])
     close(fd);
 }
 
-void column_validator_process()
+//* Uses column_validator func to validate columns within the board parameter.
+//* If valid sends [CVP_SUCCEED] msg to parent.
+//* If invalid sends [CVP_FAILED] msg to parent.
+void column_validator_process(int width, int height, char board[width][height])
 {
+    if (column_validation(width, height, board) == false)
+    {
+        /* Validation failed */
+        int fd = open(FIFO_PATH, O_WRONLY);
+        if (fd == -1)
+        {
+            /* Openning fifo fd failed */
+            printf("Opening FIFO fd failed in CVP.");
+            return;
+        }
+        if (write(fd, CVP_FAILED, sizeof(CVP_FAILED)) == -1)
+        {
+            printf("Can't write to FIFO in CVP.");
+            return;
+        }
+        close(fd);
+        return;
+    }
+    /* Validation succeed */
+    int fd = open(FIFO_PATH, O_WRONLY);
+    if (fd == -1)
+    {
+        /* Openning fifo fd failed */
+        printf("Opening FIFO fd failed in CVP.");
+        return;
+    }
+    if (write(fd, CVP_SUCCEED, sizeof(CVP_SUCCEED)) == -1)
+    {
+        printf("Can't write to FIFO in CVP.");
+        return;
+    }
+    close(fd);
 }
 
-void subRect_validator_process()
+//* Uses subRect_validator func to validate subRects within the board parameter.
+//* If valid sends [SVP_SUCCEED] msg to parent.
+//* If invalid sends [SVP_FAILED] msg to parent.
+void subRect_validator_process(int width, int height, char board[width][height], int subWidth, int subHeight)
 {
+    if (subRect_validation(width, height, board, subWidth, subHeight) == false)
+    {
+        /* Validation failed */
+        int fd = open(FIFO_PATH, O_WRONLY);
+        if (fd == -1)
+        {
+            /* Openning fifo fd failed */
+            printf("Opening FIFO fd failed in SVP.");
+            return;
+        }
+        if (write(fd, SVP_FAILED, sizeof(SVP_FAILED)) == -1)
+        {
+            printf("Can't write to FIFO in SVP.");
+            return;
+        }
+        close(fd);
+        return;
+    }
+    /* Validation succeed */
+    int fd = open(FIFO_PATH, O_WRONLY);
+    if (fd == -1)
+    {
+        /* Openning fifo fd failed */
+        printf("Opening FIFO fd failed in SVP.");
+        return;
+    }
+    if (write(fd, SVP_SUCCEED, sizeof(SVP_SUCCEED)) == -1)
+    {
+        printf("Can't write to FIFO in SVP.");
+        return;
+    }
+    close(fd);
 }
